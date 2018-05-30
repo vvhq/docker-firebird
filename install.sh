e2b6e603cbf76734103428fa1b847ab5ece78bcb
Vladimir Bykovskiy <vvhqio@gmail.com>
2018-05-31 01:53:29 +0300

    2.5-superclassic

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EF648708 && \
-echo "deb http://ppa.launchpad.net/mapopa/firebird3.0/ubuntu xenial main" > /etc/apt/sources.list.d/firebird.list && \
+echo "deb http://ppa.launchpad.net/mapopa/ppa/ubuntu xenial main" > /etc/apt/sources.list.d/firebird.list && \
 apt-get update && \
-apt-get install -y -qqq firebird${FIREBIRD_VERSION}-server && \
+apt-get install -y -qqq firebird${FIREBIRD_VERSION}-${FIREBIRD_ARCHITECTURE} && \
 apt-get autoremove -y && \
 apt-get clean all  && \
 rm -rf /var/lib/apt/lists/*
 sed -ri 's/RemoteBindAddress = localhost/RemoteBindAddress = 0.0.0.0/g' /etc/firebird/${FIREBIRD_VERSION}/firebird.conf
-sed -ri 's/#ServerMode = Super/ServerMode = '${FIREBIRD_ARCHITECTURE}'/g' /etc/firebird/${FIREBIRD_VERSION}/firebird.conf
